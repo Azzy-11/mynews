c174a17b95e1de1140bef111c66faa6201877095
EC2 Default User <ec2-user@ip-172-31-25-149.ap-southeast-2.compute.internal>
2023-09-05 12:22:25 +0000

    kagai

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(ProfileController::class)->prefix('admin')->group(function()  {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
