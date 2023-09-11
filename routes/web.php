fc991ab5b2a7e2bf1fc7de8b044fb945bffd2b86
EC2 Default User <ec2-user@ip-172-31-25-149.ap-southeast-2.compute.internal>
2023-09-11 13:40:32 +0000

    課題

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ Route::get('/', function () {
 });
 
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 Route::controller(ProfileController::class)->prefix('admin')->group(function()  {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
 });
 Auth::routes();
 
